e68d03db036b748da4444f66d4504f58d68ead8d
Szabó Richárd <richarszab56@gmail.com>
2022-07-07 20:31:05 +0200

    Add keysupport

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 let screen = document.querySelector(".calculator-screen");
 const buttons = document.querySelectorAll(".button");
 let storageOfCalculation = document.querySelector('.firstnumber');
-const resultButton = document.querySelector('[data-value = "="]');
 const clearButton = document.querySelector('.clearbutton');
-
+const decimalButton = document.querySelector('[data-value="."]');
 screen.textContent = "0";
 
 // handle operates
 function getResult(obj){
+
+    if(obj.num1 == 0 && obj.operator == "/" ){
+        return false;
+    }
+
     switch(obj.operator){
-        case '+': return obj.num1 + obj.num2;
-        case '-': return obj.num1 - obj.num2;
-        case '*' : return obj.num1 * obj.num2;
+        case '+': return (obj.num1 + obj.num2);
+        case '-': return (obj.num1 - obj.num2);
+        case '*' : return (obj.num1 * obj.num2);
         case '/': return parseFloat(obj.num1 / obj.num2).toFixed(2);
     }
 }
@@ -23,7 +27,7 @@ function calculate(calculationString){
     let calculation;
     let calculationArray = calculationString.split(' ');
 
-    // the calculation requires three members (num1, operator, num2) if
+    // the calculation requires three members (num1, operator, num2)
     if(calculationArray.length != 3) return false;
     // setting the operator then check it
 
@@ -37,7 +41,7 @@ function calculate(calculationString){
     let number2 = calculationArray[2];
 
     // last check
-    if(isNaN(number1) || isNaN(number2)){
+    if(isNaN(number1) || isNaN(number2) || number1 == ""  || number2 == ""){
         return false;
     }
 
@@ -47,55 +51,76 @@ function calculate(calculationString){
         num2: parseFloat(number2),
     }
 
+    if(!getResult(calculation)){
+        alert('You cannot divide by 0');
+        return false;
+    }
+
     return getResult(calculation);
 }
 
 
-// strore calculation members
+// strore and display the calculation members
 function store(screenDisplay, operator){
     let returnOutput = screenDisplay;
     if(operator != undefined){
 
-        if(operator === "="){
+        if(operator === "=" || operator === "Enter"){
 
             storageOfCalculation.textContent += ` ${screenDisplay.split(operator)[0]}`;
             // calculate give us a feedback if there is no result we got a false value
             if(calculate(storageOfCalculation.textContent)){
                 returnOutput = calculate(storageOfCalculation.textContent);
-            }else{
+            }else {
                 returnOutput = "0";
                 storageOfCalculation.textContent = "";
             }
 
+        }else if(operator == "."){
+
+           if(screenDisplay.includes(operator)){
+                decimalButton.disabled = true;
+           }
+
         }else{
             storageOfCalculation.textContent = `${screenDisplay.split(operator)[0]} ${operator}`;
+            decimalButton.disabled = false;
             returnOutput = "";
         }
     }
     return returnOutput;
 }
 
-// add the store function on the buttons
+// handle the display content
+function handleScreen(value){
+    if(isNaN(parseInt(value))){
+        screen.textContent += value;
+        screen.textContent = store(screen.textContent, value);
+    }else{
+        if(screen.textContent == "0"){
+            screen.textContent = value;
+        }else{
+            screen.textContent += value;
+        }
+        store(screen.textContent);
+    }
+}
 
+// add handle calculator screen funtion on buttons
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-
-        if(isNaN(parseInt(button.dataset.value))){
-            screen.textContent += button.dataset.value;
-            screen.textContent = store(screen.textContent, button.dataset.value);
-        }else{
-            if(screen.textContent == "0"){
-                screen.textContent = button.dataset.value;
-            }else{
-                screen.textContent += button.dataset.value;
-            }
-            store(screen.textContent);
-        }
-    })
+        handleScreen(button.dataset.value);
+    });
 });
 
+// keysupport
+window.addEventListener('keydown', e => {
+   handleScreen(e.key);
+});
 
+// delete all numbers 
 clearButton.addEventListener('click', () => {
     screen.textContent = "0";
     storageOfCalculation.textContent = "";
+    decimalButton.disabled = false;
 });
